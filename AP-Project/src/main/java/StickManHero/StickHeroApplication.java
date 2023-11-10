00bd8cb333e579a34c452a2432f1908940bb3db2
sahilguptasg2017 <117901246+sahilguptasg2017@users.noreply.github.com>
2023-11-11 03:50:36 +0530

    Need to increase the size of the image

--- AP-Project/src/main/java/StickManHero/StickHeroApplication.java
+++ AP-Project/src/main/java/StickManHero/StickHeroApplication.java
@@ -12,7 +12,7 @@ public class StickHeroApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(StickHeroApplication.class.getResource("StickHero.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("StickHero Game");
         stage.setScene(scene);
         stage.show();
     }
