221bc1e832187106b81ba7c0ea5cf7bc91aa07e1
sahilguptasg2017 <117901246+sahilguptasg2017@users.noreply.github.com>
2023-11-10 16:22:24 +0530

    Add files via upload

---
+++ AP-Project/src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module com.example.approject {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.example.approject to javafx.fxml;
+    exports com.example.approject;
+}
